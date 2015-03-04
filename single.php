7b38b2b9f2a54f69d9f37d1c8c5e05a03bcd4e7e
Björn Henriksson <bjarna2@gmail.com>
2015-03-04 16:48:36 +0100

    changed slideshow

--- single.php
+++ single.php
@@ -1,18 +1,15 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
-	<section>
-
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 		
 	<div class="content">
 
-	<div class="title" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>);">
+	<div class="title">
 
-		<section>
+		<section class="showcase">
 
+		<p class="date"><?php the_date(); ?></p>
 		<h1><?php the_title(); ?></h1>
 
 		<p><?php the_field("sell"); ?></p>
@@ -22,6 +19,15 @@
 		<a href="<?php the_field("url"); ?>" target="_blank" class="button-portfolio">See live</a>
 
 		<a class="button-portfolio no-margin" href="http://twitter.com/share?text=( <?php the_title(); ?> )Björn Henriksson Portfolio&url=<?php the_permalink(); ?>"><i class="fa fa-twitter"></i></a>
+
+		</section>
+
+		<section class="showcase flip">
+		
+		<a href=""><img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>" class="stretch" alt="<?php the_title(); ?> image" /></a>
+		<a href=""><img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>" class="stretch" alt="<?php the_title(); ?> image" /></a>
+		<a href=""><img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>" class="stretch" alt="<?php the_title(); ?> image" /></a>
+		<a href=""><img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>" class="stretch" alt="<?php the_title(); ?> image" /></a>
 
 		</section>
 
